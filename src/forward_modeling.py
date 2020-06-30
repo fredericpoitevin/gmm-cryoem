5707b4b26a29107997d5fed153f6dc136b8762bb
Michaël Resplandy <mmresplandy@gmail.com>
2020-06-30 15:58:56 +0200

    advances backprojection

--- src/forward_modeling.py
+++ src/forward_modeling.py
@@ -1,10 +1,9 @@
 import numpy as np
-#from scipy.spatial.transform import Rotation as R
+from scipy.spatial.transform import Rotation as R
 from scipy.interpolate import RegularGridInterpolator
-#from scipy.interpolate import griddata
-
 from scipy.interpolate import griddata, RegularGridInterpolator
-def slice_volume(vol, Rotation, method='linear'):
+
+def slice_volume(vol, Rotation, method='linear', angle_abs_mode = False):
     """
     Take a slice out of volume
     """
@@ -12,27 +11,34 @@ def slice_volume(vol, Rotation, method='linear'):
     x = np.linspace(-1,1,N)
     y = np.linspace(-1,1,N)
     z = np.linspace(-1,1,N)
-    interpolating_function = RegularGridInterpolator(points=[x,y,z],values=vol,bounds_error = False, fill_value=0, method=method)
-
+    
     grid_x, grid_y = np.meshgrid(x,y, indexing='ij')
     zeros = np.zeros((grid_x.shape))
     points = np.stack([grid_x.ravel(),grid_y.ravel(), zeros.ravel()],1)
     rot_points = Rotation.apply(points)
+    
+    if not(angle_abs_mode):
+        interpolating_function = RegularGridInterpolator(points=[x,y,z],values=vol,bounds_error = False, fill_value=0, method=method)
+        image = interpolating_function(rot_points)
 
-    image = interpolating_function(rot_points)
+    else:
+        angle_interpolating_function = RegularGridInterpolator(points=[x,y,z],values=np.angle(vol),bounds_error = False, method=method)
+        abs_interpolating_function = RegularGridInterpolator(points=[x,y,z],values=np.abs(vol),bounds_error = False, fill_value=0, method=method)
+        image = abs_interpolating_function(rot_points)*np.exp(1j*abs_interpolating_function(rot_points))
+
     return image.reshape(N,N)
 
-#def project_volume(vol, Rotation):
-#    """
-#    Given a real-space density volume, retrieve real-space projection after Rotation
-#    """
-#    vol_ft = np.fft.fftn(vol)
-#    vol_shift = np.fft.fftshift(vol_ft)
-#    im_ft = slice_volume(vol_shift, Rotation)
-#    im = np.fft.ifftshift(im_ft)
-#    projection = np.real(np.fft.ifft2(im))
+def project_volume_bis(vol, Rotation, angle_abs_mode = False):
+    """
+    Given a real-space density volume, retrieve real-space projection after Rotation
+    """
+    vol_ft = np.fft.fftn(vol)
+    vol_shift = np.fft.fftshift(vol_ft)
+    im_ft = slice_volume(vol_shift, Rotation, angle_abs_mode = angle_abs_mode)
+    im = np.fft.ifftshift(im_ft)
+    projection = np.real(np.fft.ifft2(im))
     
-#    return projection
+    return projection
 
 def rotate_volume(vol, Rotation):
     """
@@ -63,10 +69,116 @@ def project_volume(vol, Rotation):
     Given a real-space density volume, retrieve real-space projection after Rotation
     """
     rot_vol = rotate_volume(vol, Rotation)
-    rot_vol_ft = np.fft.fftn(rot_vol)
-    rot_vol_shift = np.fft.fftshift(rot_vol_ft)
-    im_ft = take_slice(rot_vol_shift)
-    im = np.fft.ifftshift(im_ft)
-    projection = np.real(np.fft.ifft2(im))
+    projection = np.sum(rot_vol, axis=2)
+    return projection
+
+
+
+
+######################################################################################################################################################################################################################################################################################################################## BACKPROJECTION ##############################################################################################################################################################################################################################################################################################################################
+
+def create_circular_mask(N, center=None, radius=None, is_volume=False):
+    if radius is None: 
+        radius = int(N/2)
+    if center is None: # use the middle of the image
+        if (is_volume):
+            center = (int(N/2), int(N/2), int(N/2))
+        else:
+            center = (int(N/2), int(N/2))
+
+    if (is_volume):        
+        Z, Y, X = np.ogrid[:N, :N, :N]
+        dist_from_center = np.sqrt((X - center[0])**2 + (Y-center[1])**2 + (Z-center[2])**2)
+    if not(is_volume):        
+        Y, X = np.ogrid[:N, :N]
+        dist_from_center = np.sqrt((X - center[0])**2 + (Y-center[1])**2)
+        
+    mask = dist_from_center <= radius
+    return mask
+
+def backprojection(images, orientations):
     
-    return 50*projection #to correct scale problem
+    N=images[0].shape[0]
+    vol = np.zeros((N,N,N),  dtype=np.complex)
+    counts = np.zeros((N,N,N))
+    mask1 = create_circular_mask(N, radius = 10)
+    
+    for i in range(len(images)):
+        #adding the contribution of each slices/images
+        rot = R.from_rotvec(-orientations[i])
+        image_i=np.fft.fftshift(np.fft.fft2(np.fft.fftshift(images[i])))
+        image_i[~mask1]=0
+        vol, counts = add_slice(vol, counts, image_i, rot)
+
+    counts[counts == 0] = 1
+    mask = create_circular_mask(N, radius = 15)
+    #return vol/counts
+    vol = vol/counts
+    vol[~mask] = 0
+    
+    return np.real(np.fft.ifftshift(np.fft.ifftn(np.fft.ifftshift(vol))))
+   
+
+def lattice_create_circular_mask(lattice, N, radius=1.):
+    """
+    Mask for vectors with real cordinates.
+    We'll use this function to work with the unit ball instead of the whole volume.
+    """
+    dist_from_center = np.sqrt((lattice[:,0])**2 + (lattice[:,1])**2 + (lattice[:,2])**2)
+    mask = (dist_from_center < radius)
+    return mask
+
+def add_slice(vol, counts, image, rot, prob = 1):
+    """
+    This function adds an image to the volume, by updating the values of the pixels arround the slice.
+    """
+    N = vol.shape[0]
+    image=image.reshape((N*N,1))
+    d2 = int(N/2)
+    
+    #building rotated lattice
+    x = np.linspace(-1,1,N)
+    y = np.linspace(-1,1,N)
+    grid_x, grid_y = np.meshgrid(x,y, indexing='ij')
+    zeros = np.zeros((grid_x.shape))
+    points = np.stack([grid_x.ravel(),grid_y.ravel(), zeros.ravel()],1)
+    rot_points = rot.apply(points)
+    
+    #coordinates of the adjacent voxels
+    points_f = (np.floor(rot_points*d2))/d2
+    points_c = (np.ceil(rot_points*d2))/d2
+    
+    #By mixing the floor values and the ceil values, we can browse all adjacent voxels
+    #we only keep the points that are inside the unit ball
+    xf, yf, zf = np.split(points_f,3,1)
+    xc, yc, zc = np.split(points_c,3,1)
+    
+    def add_for_corner(xi,yi,zi):
+        """
+        This auxilliary function add the contribution of the slice to a set of adjacent voxels
+        """
+        mask_i = lattice_create_circular_mask(np.stack((xi,yi,zi), axis=1)[:,:,0], N)
+        dist = np.stack((xi,yi,zi), axis=1)[:,:,0] - rot_points[:]
+        dist = dist[mask_i]
+        w = 1 - np.sqrt(np.sum(np.power(dist, 2), axis=1))
+        w[w<0]=0
+        w=w.reshape((-1, 1))
+        vol[(xi[mask_i]*d2+d2).astype(int),(yi[mask_i]*d2+d2).astype(int),(zi[mask_i]*d2+d2).astype(int)] += (w*image[(mask_i)]) * prob
+        counts[(xi[mask_i]*d2+d2).astype(int),(yi[mask_i]*d2+d2).astype(int),(zi[mask_i]*d2+d2).astype(int)] += w * prob
+    
+    #calling the auxilliary function for all adjacent voxels
+    add_for_corner(xf,yf,zf)
+    add_for_corner(xc,yf,zf)
+    add_for_corner(xf,yc,zf)
+    add_for_corner(xf,yf,zc)
+    add_for_corner(xc,yc,zf)
+    add_for_corner(xf,yc,zc)
+    add_for_corner(xc,yf,zc)
+    add_for_corner(xc,yc,zc)
+    
+    return vol, counts
+
+
+
+
+
